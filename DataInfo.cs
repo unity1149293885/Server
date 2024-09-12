183d84b9c9c31fe38d65cd0b255f4b695ade65f8
张子帅 <zhangzishuai_j@cyou-inc.com>
2024-09-12 17:29:12 +0800

    chore:上传代码

---
+++ DataInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Server
+{
+    public class Animal
+    {
+        public int id;
+        public string name;
+        public List<int> SkillIdList;
+        public int price;
+    }
+
+    public class People
+    {
+        public string name;
+        public Dictionary<string, string> friends;
+    }
+}
